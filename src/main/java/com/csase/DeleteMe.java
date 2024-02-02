469c5b0a831a49534fe0cc3677460f0943435c96
Nesmasabry <nesmasabry2002@gmail.com>
2024-02-02 22:05:59 +0200

    frist commit

--- src/main/java/com/csase/DeleteMe.java
+++ src/main/java/com/csase/DeleteMe.java
@@ -3,7 +3,9 @@ package com.csase;
 public class DeleteMe {
     
     public void deleteMe() {
-        System.out.println("Delete me!");
+       
+        System.out.println("Delete me!"); 
+        System.out.println("Delete meeeeeeeeee!");
     }
     
 }
